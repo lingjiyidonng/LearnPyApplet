b9677b4960e286f6094dc4eb62b68e5a809c5186
zrabbbit <1374803499@qq.com>
2021-11-11 09:34:00 +0800

    更改tabbar设置,新增页面跳转

---
+++ pages/component/course/course.js
@@ -0,0 +1,35 @@
+// pages/component/course/course.js.js
+
+var app = getApp();
+
+Component({
+
+    /* 开启全局样式设置 */
+    options: {
+        addGlobalClass: true,
+    },
+
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        name:{
+            type:String,
+            value:'教程'
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
