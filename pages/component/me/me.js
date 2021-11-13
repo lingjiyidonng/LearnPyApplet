624913cd595c02deb0d7229f706a64ebe07c0290
zrabbbit <1374803499@qq.com>
2021-11-13 16:45:18 +0800

    新增个人界面

--- pages/component/me/me.js
+++ pages/component/me/me.js
@@ -22,16 +22,21 @@ Component({
      * 组件的初始数据
      */
     data: {
-
+        user_name:'LingJiYi_Dong',
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-        click:function(){
+        toMyStar:function(){
             wx.navigateTo({
-              url: '/pages/me/me',
+              url: '/pages/my_star/my_star',
+            })
+        },
+        toMyCode:function(){
+            wx.navigateTo({
+              url: '/pages/my_code/my_code',
             })
         }
     }
