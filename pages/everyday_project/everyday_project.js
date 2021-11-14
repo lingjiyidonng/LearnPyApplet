660ba42e056eb699cc8cc7cd9c002c60c50042ea
zrabbbit <1374803499@qq.com>
2021-11-14 11:36:07 +0800

    新增我的收藏项目界面

--- pages/everyday_project/everyday_project.js
+++ pages/everyday_project/everyday_project.js
@@ -4,20 +4,5 @@ Page({
         data:{
             article : ''// 用于绑定组件的数据使用
         },
-        onClick: function (e) {
-            var url = "" + e.currentTarget.dataset.str;
-            if (url === 'Directory') {
-                wx.redirectTo({
-                    url: "../" + url + "/" + url
-                });
-            }
-            else {
-                wx.navigateTo({
-                    url: "../" + url + "/" + url
-                });
-            }
-        },
-    
-        
     })
 
--- pages/my_star/my_star.js
+++ pages/my_star/my_star.js
@@ -5,7 +5,12 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        title_item_list:[
+            "josephmisiti/awesome-machine-learning","wepe/MachineLearning","udacity/machine-learning","rasbt/python-machine-learning-book",
+        ],
+        title_item_content:[
+            "Learning frameworks, libraries and software.","Basic Machine Learning and Deep Learning","Content for Udacity's Machine Learning curriculum","The 'Python Machine Learning (1st edition)' book code repository and info resource",
+        ]
     },
 
     /**
