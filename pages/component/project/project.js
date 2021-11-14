40f4d8214c08d7876eb66e89a75c01e743cd2105
zrabbbit <1374803499@qq.com>
2021-11-14 11:06:31 +0800

    新增项目界面

--- pages/component/project/project.js
+++ pages/component/project/project.js
@@ -22,7 +22,16 @@ Component({
      * 组件的初始数据
      */
     data: {
-
+        list_num:10,
+        title_name_list:[
+            "机器学习","OpenCV","爬虫","语言识别","自然语言处理",
+        ],
+        title_item_list:[
+            "josephmisiti/awesome-machine-learning","wepe/MachineLearning","udacity/machine-learning","rasbt/python-machine-learning-book",
+        ],
+        title_item_content:[
+            "Learning frameworks, libraries and software.","Basic Machine Learning and Deep Learning","Content for Udacity's Machine Learning curriculum","The 'Python Machine Learning (1st edition)' book code repository and info resource",
+        ]
     },
 
     /**
--- pages/everyday_project/everyday_project.js
+++ pages/everyday_project/everyday_project.js
@@ -1,66 +1,23 @@
 // pages/everyday_project/everyday_project.js
+
 Page({
+        data:{
+            article : ''// 用于绑定组件的数据使用
+        },
+        onClick: function (e) {
+            var url = "" + e.currentTarget.dataset.str;
+            if (url === 'Directory') {
+                wx.redirectTo({
+                    url: "../" + url + "/" + url
+                });
+            }
+            else {
+                wx.navigateTo({
+                    url: "../" + url + "/" + url
+                });
+            }
+        },
+    
+        
+    })
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
-})
